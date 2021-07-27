9a7b1759da8513a359d29c126b93ace39d7083a1
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2021-07-27 09:54:26 +0700

    breadcrumbs rev

--- README.md
+++ README.md
@@ -28,4 +28,4 @@ Usage
 Once the extension is installed, simply use it in your code by  :
 
 ```php
-<?= \khanifzyen\stislabreadcrumbs\AutoloadExample::widget(); ?>```
+<?= \khanifzyen\stislabreadcrumbs\Breadcrumbs::widget(); ?>```
--- StBreacrumbs.php
+++ StBreacrumbs.php
@@ -5,7 +5,7 @@ namespace khanifzyen\stislabreadcrumbs;
 /**
  * This is just an example.
  */
-class Breadcrumbs extends \yii\bootstrap4\Breadcrumbs
+class StBreadcrumbs extends \yii\bootstrap4\Breadcrumbs
 {
     public function run()
     {
